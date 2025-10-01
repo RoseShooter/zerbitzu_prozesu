75f62f68f73a1e5ce2d786e7255c041d283e9416
MireiaCalvo <calvomojon.mireia@gmail.com>
2025-10-01 10:20:09 +0200

    kontaketa semaphore
    eginda eta funtzionatzen

--- MilioiSemaforoaThread/src/milioisemaforoathread/MilioiSemaforoaThread.java
+++ MilioiSemaforoaThread/src/milioisemaforoathread/MilioiSemaforoaThread.java
@@ -4,6 +4,8 @@
  */
 package milioisemaforoathread;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author mirei
@@ -14,7 +16,63 @@ public class MilioiSemaforoaThread {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Semaphore semaphore = new Semaphore(1);
+        Kontaketa kontaketa = new Kontaketa();
+
+        Runnable task = () -> {
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + " is using the resource.");
+                
+                for(int i = 0; i < 250000; i++){
+                    kontaketa.gehitu();
+                }
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+                System.out.println(Thread.currentThread().getName() + " has finisehd using the resource.");
+            }
+        };
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+        Thread thread3 = new Thread(task);
+        Thread thread4 = new Thread(task);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Kontaketa: " + kontaketa.getKontaketa());
     }
-    
+
 }
+
+class Kontaketa {
+
+    private int count = 0;
+
+    public synchronized void gehitu() {
+        count++;
+    }
+
+    public int getKontaketa() {
+        return count;
+    }
+}
+
+
+
