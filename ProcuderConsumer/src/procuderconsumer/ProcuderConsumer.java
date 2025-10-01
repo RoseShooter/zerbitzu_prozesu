26af9f7d39f6d8f693cca255e1e3223dbc6096c4
MireiaCalvo <calvomojon.mireia@gmail.com>
2025-10-01 13:19:21 +0200

    ProducerCustomer
    no se si esta bien pero funciona mas o menos

---
+++ ProcuderConsumer/src/procuderconsumer/ProcuderConsumer.java
@@ -0,0 +1,175 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procuderconsumer;
+
+/**
+ *
+ * @author mirei
+ */
+public class ProcuderConsumer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        StockStore s = new StockStore(10);
+        
+        // Create 2 producers
+        Producer p1 = new Producer(s, "Producer-1");
+        Producer p2 = new Producer(s, "Producer-2");
+        
+        // Create 3 consumers
+        Consumer c1 = new Consumer(s, "Consumer-1");
+        Consumer c2 = new Consumer(s, "Consumer-2");
+        Consumer c3 = new Consumer(s, "Consumer-3");
+        
+        try {
+            // Start all threads
+            p1.start();
+            p2.start();
+            c1.start();
+            c2.start();
+            c3.start();
+            
+            // Let the program run for 20 seconds
+            Thread.sleep(20000);
+            
+            // Interrupt all threads
+            p1.interrupt();
+            p2.interrupt();
+            c1.interrupt();
+            c2.interrupt();
+            c3.interrupt();
+            
+            // Wait for threads to finish
+            p1.join();
+            p2.join();
+            c1.join();
+            c2.join();
+            c3.join();
+            
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        System.out.println("Program completed after 20 seconds");
+    }
+    
+}
+
+class StockStore {
+    private char store[];
+    private int next;
+     
+    // Flags about store situation
+    private boolean isFull;
+    private boolean isEmpty;
+    
+    public StockStore(int lenght) {
+        store = new char[lenght];
+        next= 0;
+        isFull= false;
+        isEmpty = true;
+    }
+
+    // Method to consume characters from the buffer
+    public synchronized char consume() {
+        // Cannot consume if the buffer is empty
+        while (isEmpty == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+        }
+        // Decrement the count, as a character is being consumed
+        next--;
+        
+        char c = store[next];
+        // Check if the last character has been removed
+        if (next == 0) {
+            isEmpty = true;
+        }
+        // The store cannot be full because we just consumed
+        isFull = false;
+        notifyAll();
+ 
+        // Return the character to the consuming thread
+        return (store[next]);
+    }
+ 
+    // Method to add characters to the store
+    public synchronized void produce(char c) {
+        // Wait until there is space for another character
+        while (isFull == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+        }
+        // Add a character at the next available position
+        store[next] = c;
+        // Move to the next available position
+        next++;
+        isEmpty = false;
+        // Check if the buffer is full
+        if (next == this.store.length) {
+            isFull = true;
+        }
+        isEmpty = false;
+        notifyAll();
+    }
+    
+    public synchronized int getCurrentSize(){
+        return next;
+    }
+}
+
+class Producer extends Thread {
+    private StockStore store;
+    private final String words= "abcdefghijklmnopqrstuvxyz";
+    private String name;
+    
+    public Producer(StockStore store, String name) {
+        this.store = store;
+        this.name = name;
+    }
+    
+    public void run() {
+        while (true) {
+            // Get randomly words
+            char c = words.charAt((int) (Math.random() * words.length()));
+            // Produce
+            store.produce(c);
+            System.out.println(name + " added char '" + c + "' to store (size: " + store.getCurrentSize() + ")");
+            try {
+                // wait between 0 and 4 seconds 
+                sleep((int) (Math.random() * 4000));
+            } catch (InterruptedException e) { }
+        }
+    }
+}
+
+class Consumer extends Thread {
+    private StockStore store;
+    private String name;
+    
+    public Consumer(StockStore store, String name) {
+        this.store = store;
+        this.name = name;
+    }
+    
+    public void run() {
+        while (true) {
+            // Consume
+            char c= store.consume();
+            System.out.println(name + " got char '" + c + "' from store (size:" + store.getCurrentSize() + ")");
+            try {
+                // wait between 0 and 4 seconds 
+                sleep((int) (Math.random() * 4000));
+            } catch (InterruptedException e) { 
+                break;
+            }
+        }
+    }
+}
